80c7156b0ae36e973fc5ed12f71cdcedfdc6d7c2
Ettore Luglio <ettore.tamadrum@gmail.com>
2017-08-24 20:16:31 -0300

    iTrainer for IOS and web

---
+++ iTrainerIOS/iTrainerIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTrainerIOS
+//
+//  Created by Ettore Luglio on 24/08/17.
+//  Copyright © 2017 tamadrum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
